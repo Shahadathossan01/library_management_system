2f72ac5f70713b6f48dffc2cbc8f81b43bfe9e85
shahadat hossen <hossantopu@gmail.com>
2025-04-21 20:31:02 +0600

    add login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,11 @@
 const router=require('express').Router()
 
-router.post('/register',(req,res)=>{
-    res.status(200).json({message:'register route'})
+router
+.post('/register',(req,res)=>{
+    res.status(200).json({message:'check register route'})
+})
+.post('/login',(req,res)=>{
+    res.status(200).json({message:'check login route'})
 })
 
 module.exports=router;
