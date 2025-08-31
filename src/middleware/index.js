63198ca7e2ac1d7ec3a1405c0cdac856f38a6b85
topu <topu@gmail.com>
2025-09-01 00:20:29 +0600

    Update: db url (use mongodb atlas)

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -8,7 +8,7 @@ const swaggerDoc = YAML.load("./swagger.yaml");
 const applyMiddleware = (app) => {
   app.use(express.json());
   app.use(morgan("dev"));
-  app.use(cors({ origin: "*" }));
+  app.use(cors());
   app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
   app.use(
     OpenApiValidator.middleware({
