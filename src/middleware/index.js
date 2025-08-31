67a9f0567bb99a08e4b90bd48cb8b59dec02fb73
topu <topu@gmail.com>
2025-09-01 00:06:04 +0600

    Update: db url (use mongodb atlas)

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,21 +1,22 @@
-const express=require('express')
-const morgan=require('morgan')
-const cors=require('cors')
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const OpenApiValidator = require('express-openapi-validator');
-const swaggerDoc=YAML.load('./swagger.yaml')
-const applyMiddleware=(app)=>{
-    app.use(express.json())
-    app.use(morgan('dev'))
-    app.use(cors())
-    app.use('/docs',swaggerUI.serve, swaggerUI.setup(swaggerDoc))
-    app.use(
-        OpenApiValidator.middleware({
-            apiSpec:'./swagger.yaml',
-            ignorePaths: /\/api\/v1\/books(\/\d+)?|\/api\/v1\/books\/|\/api\/v1\/users(\/\d+)?/,
-        })
-    )
-}
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const OpenApiValidator = require("express-openapi-validator");
+const swaggerDoc = YAML.load("./swagger.yaml");
+const applyMiddleware = (app) => {
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(cors({ origin: "*" }));
+  app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: "./swagger.yaml",
+      ignorePaths:
+        /\/api\/v1\/books(\/\d+)?|\/api\/v1\/books\/|\/api\/v1\/users(\/\d+)?/,
+    })
+  );
+};
 
-module.exports=applyMiddleware;
+module.exports = applyMiddleware;
