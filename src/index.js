871d435f056a0a08800f9f071b5319af7860bbed
topu <topu@gmail.com>
2025-08-31 23:36:28 +0600

    Update: db url (use mongodb atlas)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
-const http=require('http')
-const app=require('./app')
-const { connectDB } = require('./db')
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+const { connectDB } = require("./db");
 
-const server=http.createServer(app)
-const port=process.env.PORT || 5000
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
-const main=async()=>{
-    try{
-        await connectDB()
-        server.listen(port,async()=>{
-            console.log(`Server is running in port: ${port}`)
-        })
-    }catch(e){
-        console.log('Database Error')
-        console.log(e)
-    }
-}
+const main = async () => {
+  try {
+    await connectDB();
+    server.listen(port, "0.0.0.0", async () => {
+      console.log(`Server is running in port: ${port}`);
+    });
+  } catch (e) {
+    console.log("Database Error");
+    console.log(e);
+  }
+};
 
-main()
+main();
